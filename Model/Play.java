5a5281fca5ca68ff9c8a21eb12469ac6854c2d7d
Limantara <edwin@limantara.me>
2015-04-12 11:17:52 -0700

    Added test case

--- Model/Play.java
+++ Model/Play.java
@@ -12,7 +12,31 @@ public class Play
 
 	public static void test1()
 	{
-
+		/*
+		 * Go to http://www.coolmath-games.com/0-mancala, play a game, and create
+		 * a test case based on it.
+		 */
+		Mancala myMancala = new Mancala();
+		mancala.selectPit(Mancala.P1, 1);
+		mancala.selectPit(Mancala.P2, 10);
+		mancala.selectPit(Mancala.P2, 12);
+		mancala.selectPit(Mancala.P1, 1);
+		mancala.selectPit(Mancala.P2, 8);
+		mancala.selectPit(Mancala.P1, 3);
+		mancala.selectPit(Mancala.P2, 13);
+		mancala.selectPit(Mancala.P1, 1);
+		mancala.selectPit(Mancala.P2, 9);
+		mancala.selectPit(Mancala.P1, 1);
+		mancala.selectPit(Mancala.P2, 13);
+		mancala.selectPit(Mancala.P2, 8);
+		mancala.selectPit(Mancala.P1, 3);
+		mancala.selectPit(Mancala.P2, 11);
+		mancala.selectPit(Mancala.P1, 3);
+		mancala.selectPit(Mancala.P2, 13);
+		mancala.selectPit(Mancala.P2, 14);
+		mancala.selectPit(Mancala.P2, 10);
+		mancala.selectPit(Mancala.P1, 1);
+		mancala.selectPit(Mancala.P2, 10);
 	}
 
 	public static void test2()
