6defef106d47f43c168c40970163ee096e9680c3
Limantara <edwin@limantara.me>
2015-04-12 11:33:19 -0700

    Implemented methods and gave proper Javadocs

--- Model/Pit.java
+++ Model/Pit.java
@@ -3,9 +3,21 @@ public class Pit
 	private int num_stone;
 	private int INITIAL_NUM_STONE = 3;
 
+	/**
+	 * Constructs a new Pit with 3 stones.
+	 */
 	public Pit()
 	{
-		
+		this(INITIAL_NUM_STONE);
+	}
+
+	/**
+	 * Constructs a new Pit with custom num_stone.
+	 * @param num_stone the number of stones in this Pit to begin with.
+	 */
+	public Pit(int num_stone)
+	{
+		this.num_stone = num_stone;
 	}
 
 	public void incStone()
