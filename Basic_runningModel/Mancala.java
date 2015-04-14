2c0fb77084b4c4c64f0439aa933bd0238fccc3b7
fantasysukun <188454747@qq.com>
2015-04-14 00:05:44 -0700

    Update Mancala.java
    update Mancala GUI component

--- Basic_runningModel/Mancala.java
+++ Basic_runningModel/Mancala.java
@@ -2,6 +2,7 @@ import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 
 import javax.swing.Icon;
@@ -25,7 +26,22 @@ public class Mancala{
 		Rectangle body = new Rectangle(x,y,width, height);
 		g2.draw(body);
 	// Create the parts of this car and draw them here.
-		g2.drawString(stones + "", x + width / 2, y + 20);
+		//g2.drawString(stones + "", x + width / 2, y + 20);
+		
+		int Xcircle = 0;
+		int Ycircle = 0;
+		for(int i=0; i<stones; i++)
+		{
+			//create Pit inside the Rectangle, MAX Pit will be 36.
+			Ellipse2D.Double circle = new Ellipse2D.Double (x + Xcircle, y + Ycircle, 10, 10); 
+			g2.fill(circle);
+			Xcircle += 10;
+			if(Xcircle == 60)
+			{
+				Xcircle = 0;
+				Ycircle += 10;
+			}
+		}
 	}
 	
 	public int getWidth() { return width; }
