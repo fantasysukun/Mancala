f8456589cc9e76990724edd06de4e62fc0c91db8
fantasysukun <188454747@qq.com>
2015-04-16 09:09:30 -0700

    testing done

--- MancalaSystem.java
+++ MancalaSystem.java
@@ -7,8 +7,5 @@ public class MancalaSystem
 		Board board = new Board();
 		BoardFrame test = new BoardFrame(board);
 		
-		//testing the GibHub is working or not
-		String s = "testing the GibHub is working or not";
-		System.out.println(s);
 	}
 }
